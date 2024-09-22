176d7769c00ee524d40eb96a162a1faf457ab5fc
tofu1102 <p3mf5cxkh2itof@gmail.com>
2024-09-22 16:51:09 +0900

    STATIC_ROOTの修正

--- FTSApp/FTSApp/settings.py
+++ FTSApp/FTSApp/settings.py
@@ -130,7 +130,7 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 MEDIA_URL = '/media/'
